71d970af156c07de16a6da2609caeb4e404881b3
Michael J. Sullivan <sully@msully.net>
2018-10-19 14:53:30 -0700

    Split the linux builds into 3

--- build-linux-wheels.sh
+++ build-linux-wheels.sh
@@ -5,14 +5,12 @@
 
 cd /io/mypy
 
+VER="${1//.}"
+PYBIN="/opt/python/cp${VER}-cp${VER}m/bin"
+
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
-    if [ $(echo "${PYBIN}" | grep -o '[[:digit:]][[:digit:]]' | head -n 1) -ge 35 ]; then
-	# only builds on Python 3.5 and newer
-	"${PYBIN}/pip3" install -r /io/mypyc/external/mypy/test-requirements.txt
-	PYTHONPATH=/io/mypyc CC=/opt/llvm/bin/clang MYPYC_OPT_LEVEL=3 "${PYBIN}/python3" setup.py --use-mypyc bdist_wheel
-  fi
-done
+"${PYBIN}/pip3" install -r /io/mypyc/external/mypy/test-requirements.txt
+PYTHONPATH=/io/mypyc CC=/opt/llvm/bin/clang MYPYC_OPT_LEVEL=3 "${PYBIN}/python3" setup.py --use-mypyc bdist_wheel
 
 # Bundle external shared libraries into the wheels
 for whl in dist/*.whl; do

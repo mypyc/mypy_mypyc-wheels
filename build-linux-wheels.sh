a64adec9953bac80b43dad17dc21b2ef41bea4f0
Michael J. Sullivan <sully@msully.net>
2019-01-29 17:17:18 -0800

    Fix linux install

--- build-linux-wheels.sh
+++ build-linux-wheels.sh
@@ -2,6 +2,8 @@
 
 # Unpack a modern clang version
 (cd / && curl -L https://github.com/mypyc/mypy_mypyc-wheels/releases/download/llvm/llvm-centos-5.tar.gz | tar xzf -)
+# Install mypyc
+"${PYBIN}/pip3" install -r mypyc-requirements.txt
 
 cd /io/mypy
 
@@ -9,7 +11,6 @@ VER="${1//.}"
 PYBIN="/opt/python/cp${VER}-cp${VER}m/bin"
 
 # Compile wheels
-"${PYBIN}/pip3" install -r mypyc-requirements.txt
 CC=/opt/llvm/bin/clang MYPYC_OPT_LEVEL=0 "${PYBIN}/python3" setup.py --use-mypyc bdist_wheel
 
 # Bundle external shared libraries into the wheels

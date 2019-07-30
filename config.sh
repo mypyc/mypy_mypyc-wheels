cd26db665ca375215ff5ccb5d4c8b5d653bff668
Michael J. Sullivan <sully@msully.net>
2019-07-30 13:09:09 -0700

    Only install from  mypyc-requirements.txt if it exists
    This will let us handle the transition to a merged repo

--- config.sh
+++ config.sh
@@ -1,6 +1,8 @@
 function pre_build {
     pip install virtualenv
-    pip install -r mypy/mypyc-requirements.txt
+    if [ -f mypy/mypyc-requirements.txt ]; then
+	pip install -r mypy/mypyc-requirements.txt
+    fi
 }
 
 function run_tests {

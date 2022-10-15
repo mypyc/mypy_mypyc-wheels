898c9cd5786782bd16ebfd9b27286e65a41a1f73
Ethan Smith <ethan@ethanhs.me>
2022-10-15 15:59:25 -0700

    Remove unused reading of build_wheels.py, read requirements from mypy checkout

--- wasm_test.js
+++ wasm_test.js
@@ -2,7 +2,7 @@
 const { loadPyodide } = require("pyodide");
 const process = require("process");
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 
 
 const EXPECTED_ERROR = `test.py:1: error: Unsupported operand types for + ("int" and "str")
@@ -14,21 +14,17 @@ async function test_mypy() {
   let pyodide = await loadPyodide();
   await pyodide.loadPackage("micropip")
   let micropip = pyodide.pyimport("micropip");
-  await micropip.install([
-    "typing_extensions>=3.10",
-    "mypy_extensions>=0.4.3",
-    "tomli>=1.1.0",
-  ]);
+  let requirements = fs.readFileSync("mypy/mypy-requirements.txt", {encoding: "utf8"});
+  await Promise.all(
+    requirements.split("\n").map(async (requirement) => {
+      if (!requirement.startsWith("#") && requirement != "") {
+        await micropip.install([requirement]);
+      }
+    })
+  );
   const dist_dir = "mypy/dist";
   const files = await fs.promises.readdir(dist_dir);
   await pyodide.loadPackage(path.join(dist_dir, files[0]));
-  fs.readFile('build_wheel.py', 'utf8', function(err, data) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    pyodide.FS.writeFile('build_wheel.py', data);
-  });
   pyodide.runPython(`
     with open('test.py', 'w') as f:
         f.write("1 + ''")
